747faa708bddc9c21a012ef3a0fd1676929d48d4
johnakon <john.akonya@gmail.com>
2020-08-17 16:06:31 +0300

    created the insertMongodb function to insert data into the db

--- test.js
+++ test.js
@@ -1,19 +1,65 @@
 // request is a module that makes http calls easier
-const request = require("request");
+const request = require('request');
+
+// connect to mongodb
+const MongoClient = require('mongodb');
+
+// connection string to the database
+const dsn = 'mongodb://localhost:37017/maxcoin';
 
 // Generic function that fetches the closing bitcoin dates of the last month from a public API
 function fetchFromAPI(callback) {
-  // We are using fat arrow (=>) syntax here. This is a new way to create anonymous functions in Node
-  // Please review the Node.js documentation if this looks unfamiliar to you
-  request.get(
-    "https://api.coindesk.com/v1/bpi/historical/close.json",
-    (err, raw, body) => {
-      return callback(err, JSON.parse(body));
-    }
+  request.get('https://api.coindesk.com/v1/bpi/historical/close.json', (err, raw, body) =>
+    callback(err, JSON.parse(body))
   );
 }
 
-fetchFromAPI((err, data) => {
+// function insertMongodb
+function insertMongodb(collection, data) {
+  const promisedInserts = [];
+
+  // to traverse the list, Objects.keys is used
+  Object.keys(data).forEach((key) => {
+    // push a promise into the promisedInserts array
+    promisedInserts.push(
+      collection.insertOne({
+        // what is to be stored
+        date: key,
+        value: data[key],
+      })
+    );
+  });
+  // execute promises
+  return Promise.all(promisedInserts);
+}
+
+MongoClient.connect(dsn, (err, db) => {
   if (err) throw err;
-  console.log(data);
+  // eslint-disable-next-line no-console
+  console.log('connected to server');
+
+  // eslint-disable-next-line no-shadow
+  fetchFromAPI((err, data) => {
+    if (err) throw err;
+    // eslint-disable-next-line no-console
+    // console.log(data);
+
+    // define a collection to be written into
+    // const collection = db.collection('value');
+    const collection = db.db('maxcoin').collection('value');
+
+    insertMongodb(collection, data.bpi)
+      .then((result) => {
+        // eslint-disable-next-line no-console
+        console.log(`successfuly inserted ${result.length} documents into mongodb`);
+        // eslint-disable-next-line no-unused-expressions
+        db.close;
+      })
+      // eslint-disable-next-line no-shadow
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.log(err);
+        process.exit();
+      });
+  });
 });
